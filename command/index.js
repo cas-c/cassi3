1fb572f635a4e46806602cc440667da83802f044
gloss-water <gloss.water@gmail.com>
2018-03-11 22:22:16 -0600

    clean up command to avoid errors in dms and break info specific logic into its own block

--- command/index.js
+++ command/index.js
@@ -1,35 +1,39 @@
 const info = require('./info');
 
 const command = (text, message, client) => {
-    const nyan = client.guilds.first()
-    let user = undefined;
+    const nyan = client.guilds.first();
+    if (text.startsWith('info')) {
+        if (message.channel.type === 'dm') return 'This command is only available in guilds.';
 
-    if (message.mentions.members !== {}) {
-        // tries to find the member version if there is a mention
-        user = message.mentions.members.first();
-    } else {
-        // tries to find the user version if there is a mention, then creates member
-        user = client.guilds.first().members.get(message.mentions.users.first().id);
-    }
-    if (user === undefined) {
-        const query = text.split(' ')[1];
-        if (query === undefined) {
-            user = message.member;
+        let user = undefined;
+        if (message.mentions.members !== {}) {
+            // tries to find the member version if there is a mention
+            user = message.mentions.members.first();
         } else {
-            let search = nyan.members.get(query);
-            if (search === undefined) {
-                search = nyan.members.find('username', query);
-                if (!search) {
-                    return 'Sorry, I can\'t find that user yet.';
+            // tries to find the user version if there is a mention, then creates member
+            user = client.guilds.first().members.get(message.mentions.users.first().id);
+        }
+        if (user === undefined) {
+            const query = text.split(' ')[1];
+            if (query === undefined) {
+                user = message.member;
+            } else {
+                let search = nyan.members.get(query);
+                if (search === undefined) {
+                    search = nyan.members.find('username', query);
+                    if (!search) {
+                        return 'Sorry, I can\'t find that user yet.';
+                    }
                 }
+                user = search;
             }
-            user = search;
+        }
+        if (user) {
+            return info(user).setFooter('Generated for ' + message.member.displayName);
+        } else {
+            return 'Sorry, I can\'t find this user yet.';
         }
     }
-    if (user && text.startsWith('info')) {
-        return info(user).setFooter('Generated for ' + message.member.displayName);
-    }
-    return 'Sorry, I can\'t find that user yet.';
 }
 
 module.exports = command;
