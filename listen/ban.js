0eaf787df60a1b1215f88d76fb7e25146862cab9
gloss-water <gloss.water@gmail.com>
2018-03-11 22:14:02 -0600

    removing console logs

--- listen/ban.js
+++ listen/ban.js
@@ -1,9 +1,7 @@
 const { simpleEmbed } = require('../util');
 
 const join = (type, user) => {
-        console.log(type);
     if (type === 'add') {
-        console.log(user);
         return simpleEmbed(user, 'Banning User');
     } else {
         return simpleEmbed(user, 'Unbanning User');
