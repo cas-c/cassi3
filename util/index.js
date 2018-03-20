0ee85a6e6953a9bd678eb7f09b0baead43847781
gloss-water <gloss.water@gmail.com>
2018-03-19 20:03:23 -0600

    adding a true array shuffle to utils

--- util/index.js
+++ util/index.js
@@ -28,9 +28,26 @@ const simpleEmbed = (user, message) => new RichEmbed()
     .addField('User', user.username + '#' + user.discriminator)
     .setThumbnail(user.displayAvatarURL);
 
+
+// "True" shuffle of an array (Knuth-Fisher-Yates shuffle)
+const shuffleArray = array => {
+    let currentIndex = array.length;
+    let temp;
+    let randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random * currentIndex);
+        currentIndex -= 1;
+        temp = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+    return array;
+}
+
 module.exports = {
     censorship,
     notification,
     userInfo,
-    simpleEmbed
+    simpleEmbed,
+    shuffleArray
 };
