356e4a159b874ca0c7541aacdccf09cdea3a5c8d
gloss-water <gloss.water@gmail.com>
2018-04-17 01:26:28 -0600

    catch guilds missing from config

--- util/index.js
+++ util/index.js
@@ -50,18 +50,21 @@ const field = (name, value, inline) => { return { name, value, inline }; };
 
 const wordIn = (array, text) => array.some(word => text.includes(word));
 
-const getHomeChannel = (client, id) => {
-    const channel = client.channels.get(id);
-    if (channel) return channel;
-    return {
-        send: () => {
-            console.error('Error: Attempted to send a message to server with no home channel. Update your configs.');
+const getHomeChannel = (client, guildBlock) => {
+    if (!guildBlock) {
+        return {
+            send: () => {
+                console.error('Error: Attempted to send a message to server with no home channel. Update your configs.');
+            }
         }
     }
+    const channel = client.channels.get(guildBlock.home);
+    if (channel) return channel;
+
 }
-const getHomeChannelFromMessage = message => getHomeChannel(message.client, config.discord.guilds[message.guild.id].home);
-const getHomeChannelFromMember = member => getHomeChannel(member.client, config.discord.guilds[member.guild.id].home);
-const getHomeChannelFromGuild = guild => getHomeChannel(guild.client, config.discord.guilds[guild.id].home);
+const getHomeChannelFromMessage = message => getHomeChannel(message.client, config.discord.guilds[message.guild.id]);
+const getHomeChannelFromMember = member => getHomeChannel(member.client, config.discord.guilds[member.guild.id]);
+const getHomeChannelFromGuild = guild => getHomeChannel(guild.client, config.discord.guilds[guild.id]);
 
 module.exports = {
     censorship,
