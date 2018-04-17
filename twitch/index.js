03c9a5d3dc5f41c031bfd4953e4122fc0fc8a45b
gloss-water <gloss.water@gmail.com>
2018-04-17 13:21:50 -0600

    add twitch moderation features

--- twitch/index.js
+++ twitch/index.js
@@ -3,13 +3,16 @@ const Client = require('twitch-js').client;
 const config = require('../config');
 const options = config.twitch;
 const discordOptions = config.discord;
-const { field } = require('../util');
+const { field, wordIn } = require('../util');
+
+const bannedMessage = message => wordIn(config.twitchMod.bannedPhrases, message.toLowerCase());
+const timeoutMessage = message => wordIn(config.twitchMod.timeoutPhrases, message.toLowerCase());
 
 class TwitchClient extends Client {
     constructor(discordClient) {
         super(options);
         this.dClient = discordClient;
-        this.home = discordClient.channels.get(config.twitchReporting);
+        this.home = discordClient.channels.get(config.twitchMod.reporting);
         this.on('logon', () => {
             this.home.send('Cassie has logged into Twitch as 0xmildred!');
         })
@@ -52,6 +55,26 @@ class TwitchClient extends Client {
                 ]
             }));
         });
+        this.on('message', (channel, userstate, message, self) => {
+            if (self) return;
+            switch (userstate['message-type']) {
+                case 'chat':
+                    if (bannedMessage(message)) {
+                        this.ban('nyannersz', userstate['username'], `Virus link or banned phrase: ${message}`)
+                            .then(({ channel, username, reason }) => {
+                                console.log('Auto banned user for virus link in nyannersz.');
+                            })
+                            .catch(err => console.log(`Tried to ban ${userstate['username']} but was unable to. Reason: ${err}`));
+                    }
+                    if (timeoutMessage(message)) {
+                        this.timeout('nyannersz', userstate['username'], 600, `Disallowed phrase: ${message}.`)
+                            .then(({ channel, username, reason }) => {
+                                console.log('Timed out user for virus link in nyannersz.');
+                            })
+                            .catch(err => console.log(`Tried to timeout ${userstate['username']} but was unable to. Reason: ${err}`));
+                    }
+            }
+        })
     }
 }
 
