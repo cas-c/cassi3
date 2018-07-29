1c292114e6e9639ec9c3643fc0d88d61f2a2a515
gloss-water <gloss.water@gmail.com>
2018-07-28 22:12:58 -0600

    really basic environment check

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ client.once('ready', () => {
 
 client.on('message', handlers.message);
 
-client.on('messageDelete', m => {
-    if (wordIn(config.discord.censorship.exemptions, m.author.id)) return;
-    getHomeChannelFromMessage(m).send(listen.delete(m));
-});
-client.on('guildMemberAdd', m => getHomeChannelFromMember(m).send(listen.join(m)));
-client.on('guildMemberRemove', m => getHomeChannelFromMember(m).send(listen.part(m)));
-client.on('guildBanAdd', (g, m) => getHomeChannelFromGuild(g).send(listen.ban('add', m)));
-client.on('guildBanRemove', (g, m) => getHomeChannelFromGuild(g).send(listen.ban('remove', m)));
-client.on('disconnect', () => console.warn('Cassi3 disconnected from Discord.'));
-client.on('reconnecting', () => console.warn('Cassi3 reconnecting to Discord.'));
-client.on('error', console.error);
-client.on('warn', console.warn);
+if (config.env !== 'dev') {
+    client.on('messageDelete', m => {
+        if (wordIn(config.discord.censorship.exemptions, m.author.id)) return;
+        getHomeChannelFromMessage(m).send(listen.delete(m));
+    });
+    client.on('guildMemberAdd', m => getHomeChannelFromMember(m).send(listen.join(m)));
+    client.on('guildMemberRemove', m => getHomeChannelFromMember(m).send(listen.part(m)));
+    client.on('guildBanAdd', (g, m) => getHomeChannelFromGuild(g).send(listen.ban('add', m)));
+    client.on('guildBanRemove', (g, m) => getHomeChannelFromGuild(g).send(listen.ban('remove', m)));
+    client.on('disconnect', () => console.warn('Cassi3 disconnected from Discord.'));
+    client.on('reconnecting', () => console.warn('Cassi3 reconnecting to Discord.'));
+    client.on('error', console.error);
+    client.on('warn', console.warn);
+}
 
 client.login(config.auth.token);
-
-
